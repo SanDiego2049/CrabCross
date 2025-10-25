d23184294bfe393d4c61a2c3eaa4db3b59a1729f
Oreoluwa Alaba <alabaoreoluwa@gmail.com>
2025-10-25 17:42:09 +0100

    removed our team

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -13,7 +13,7 @@ const About = () => {
         <Banner />
         <Stats />
         <AboutDetails />
-        <OurTeam />
+        {/* <OurTeam /> */}
       </main>
       <Footer />
     </div>
